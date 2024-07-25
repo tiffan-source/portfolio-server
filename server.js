1ce20baed51ced061592dc32105e7ba3f6c441d4
tiffan-source <zohoungbogboanlyou@gmail.com>
2024-07-25 02:16:09 +0200

    v0.1

--- server.js
+++ server.js
@@ -9,19 +9,33 @@ const app = express();
 console.log(process.env.LINK_DB);
 const sql = postgres(process.env.LINK_DB);
 
+app.get("/drop", async (req, res) => {
+    await sql`DROP TABLE IF EXISTS visits`;
+    res.json({ message: "Table dropped" });
+});
+
 app.get("/count", async (req, res) => {
     await sql`
     CREATE TABLE IF NOT EXISTS visits (
         id SERIAL PRIMARY KEY,
-        created_at TIMESTAMPTZ DEFAULT NOW()
+        created_at TIMESTAMPTZ DEFAULT NOW(),
         name VARCHAR(255) NOT NULL
     );
-    `
-    const result = await sql`INSERT INTO visits (name) VALUES ('${os.hostname()}') RETURN`;
+    `;
+    const result = await sql`
+        INSERT INTO visits (name) 
+        VALUES (${os.hostname()}) 
+        RETURNING id
+    `;
 
     res.json({ count: result.length });
 });
 
+app.get("/list", async (req, res) => {
+    const visits = await sql`SELECT * FROM visits`;
+    res.json(visits);
+})
+
 app.listen(3001, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running on http://localhost:3001");
 });
